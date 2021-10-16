f6dbcff77e5e17a6f6f49ba6e9e54038db8d64c1
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-10-16 21:05:08 +1000

    Added 'filter'

--- src/environment.py
+++ src/environment.py
@@ -1,11 +1,15 @@
 #shape = [x, y, x2, y2]
-environment = 'tube'
+environment = 'filter'
 
 #Initialise lists to off screen
 shapelist = [-100, -100, -100, -100]
 shapelist2 = [-100, -100, -100, -100]
 shapelist3 = [-100, -100, -100, -100]
 shapelist4 = [-100, -100, -100, -100]
+shapelist5 = [-100, -100, -100, -100]
+shapelist6 = [-100, -100, -100, -100]
+shapelist7 = [-100, -100, -100, -100]
+shapelist8 = [-100, -100, -100, -100]
 
 if environment == 'demo':
     shapelist = [750, 500, 1920, 900]
@@ -20,7 +24,23 @@ elif environment == 'pool':
     shapelist = [384, -10.5, 1536, -10.5]
     shapelist2 = [384, 250, 384, -10.5]
     shapelist3 = [1536, -10.5, 1536, 250]
-ShapeList = [ shapelist, shapelist2, shapelist3, shapelist4]
+elif environment == 'tube':
+    shapelist = [900, -10.5, 1000, -10.5]
+    shapelist2 = [900, -10.5, 900, 600]
+    shapelist3 = [1000, -10.5, 1000, 600]
+elif environment == 'filter':
+    #pool area under filter
+    shapelist = [384, -10.5, 1536, -10.5]
+    shapelist2 = [384, 250, 384, -10.5]
+    shapelist3 = [1536, -10.5, 1536, 250]
+    #filter pipe 
+    shapelist4 = [910, 125, 910, 500] 
+    shapelist5 = [990, 125, 990, 500]
+    #filter head
+    shapelist6 = [910, 500, 775, 720]
+    shapelist7 = [990, 500, 1125, 720]
+
+ShapeList = [ shapelist, shapelist2, shapelist3, shapelist4, shapelist5, shapelist6, shapelist7, shapelist8]
 Shape_Amount = len(ShapeList)
 
 
