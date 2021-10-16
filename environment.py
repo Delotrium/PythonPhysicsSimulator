0d00f7c4f1facda18a925f7caa4cfe1db9681eaa
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-10-16 18:30:43 +1000

    added demo environment data

--- environment.py
+++ environment.py
@@ -1,1 +1,11 @@
 
+#shape = [x, y, x2, y2]
+
+shapelist = [750, 500, 1920, 900]
+shapelist2 = [1000, 200, 1200, 50]
+shapelist3 = [100, 300, 1000, 200]
+shapelist4 = [-200, 500, 100, 300]
+
+ShapeList = [ shapelist, shapelist2, shapelist3, shapelist4]
+Shape_Amount = len(ShapeList)
+
